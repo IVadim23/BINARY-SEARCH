fa1bf2746243ffe16024b7bd9f5aef41c9d2a241
IVadim23 <inazrski@gmail.com>
2024-08-18 00:52:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Binary_Search 
+{
+    public class Program
+    {
+
+
+    static int[] arr = { 3, 4, 56, 78, 90 };
+
+        static int BSearch(int[] arr, int x)
+        {
+        int left = 0;
+        int right = arr.Length - 1;
+        while (left <= right)
+            {
+            int middle = (left + right) / 2;
+            if (arr[middle] == x)
+                return middle;
+
+            if (x < arr[middle])
+                {
+                right = middle - 1;
+                }
+            else
+                {
+                left = middle + 1;
+                }
+            }
+        return -1;
+        }
+
+    static void Main(string[] args)
+        {
+
+        Console.WriteLine(BSearch(arr, 56));
+
+        }
+    }
+}
